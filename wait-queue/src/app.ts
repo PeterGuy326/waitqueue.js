b2117bf6a29b5adcb5e666a053c7958b27790e81
FallingKids <739820174@qq.com>
2024-01-18 20:00:30 +0800

    日志函数修改正确

--- wait-queue/src/app.ts
+++ wait-queue/src/app.ts
@@ -6,17 +6,21 @@ import { cronInit } from './middleware/timer'
 import { queueRoutes } from './routes/queue'
 import { ContextExtensional } from '../type/middleware/context'
 import { schedulerRoutes } from './routes/scheduler'
-require('dotenv').config();
+import { controllerInsMiddleware } from './middleware/controllerIns'
+require('dotenv').config()
 
 const app = new Koa<{}, ContextExtensional>()
 
 app.use(bodyParser())
-app.use(pino)
+app.use(pino())
 app.use((ctx, next) => cronInit(ctx, next))
 
 const router = new Router<{}, ContextExtensional>({
 	prefix: 'waitqueue',
 })
+
+router.use(controllerInsMiddleware) // 应用中间件
+
 // 集成子模块路由
 router.use('/scheduler', schedulerRoutes.routes())
 router.use('/queue', queueRoutes.routes())
--- wait-queue/src/routes/queue.ts
+++ wait-queue/src/routes/queue.ts
@@ -1,13 +1,12 @@
 import Router from 'koa-router'
 import * as QueueControllerType from '../../type/controller/queue'
-import { controllerInsMiddleware } from '../middleware/controllerIns'
 import { ContextExtensional } from '../../type/middleware/context'
 import response from '../utils/response'
 
 const queueRoutes = new Router<{}, ContextExtensional>()
-queueRoutes.use(controllerInsMiddleware) // 应用中间件
 
 queueRoutes.post('/newQueue', async (ctx) => {
+	console.log(1111111)
 	try {
 		const res = await ctx.queueController.newQueue(ctx.request.body as QueueControllerType.NewQueueKeyReq)
 		response.success(ctx, res)
